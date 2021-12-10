45b775dbf9e47e0cbe2cd63a8b1aeb54fd34c263
wreck888 <DChui88@gmail.com>
2021-12-09 21:44:50 -0600

    v1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
+import Character from './components/Character'
+
+const StyledDiv = styled.div`
+
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([])
+  
 
+  // const openDetails = id => {
+  //   setCurrentFriendId(id)
+  // }
+
+  // const closeDetails = () => {
+  //   setCurrentFriendId(null)
+  // }
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+      .then(res => {
+        console.log(res.data);
+        setCharacters(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, [])
 
   return (
-    <div className="App">
+    <StyledDiv className="App">
       <h1 className="Header">Characters</h1>
-    </div>
+      {/* {characters.map((person, index) => (
+      <Character key={`${index}`}/>
+      ))
+      } */}
+    {characters.map((person, index) => (
+       <Character key ={index} person={person}/> ))
+    }
+    </StyledDiv>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+display: flex;
+border: 2px solid black;
+border-radius: 8px;
+justify-content: space-between;
+flex-wrap: wrap;
+
+
+`
+
+const Character = (props) => {
+    console.log(props)
+    return (
+        <StyledDiv className='friend'>
+            <h2>{props.person.name}</h2>
+            <h3>{props.person.birth_year}</h3>
+        </StyledDiv>
+  )
+}
+
+export default Character;
